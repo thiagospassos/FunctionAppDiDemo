ab3a7e5c1dd23ca368c290ee3313b206b613ee64
Thiago Passos <thiago@passos.com.au>
2018-04-26 01:27:35 +1000

    configuring DI

---
+++ FunctionAppDiDemo/AutofacConfig.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using AzureFunctions.Autofac.Configuration;
+using FunctionAppDiDemo.Services;
+
+namespace FunctionAppDiDemo
+{
+    public class AutofacConfig
+    {
+        public AutofacConfig()
+        {
+            DependencyInjection.Initialize(builder =>
+            {
+                builder.RegisterType<ServiceOne>().As<IServiceOne>();
+            });
+
+        }
+    }
+}
---
+++ FunctionAppDiDemo/Services/ServiceOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppDiDemo.Services
+{
+    public class ServiceOne : IServiceOne
+    {
+        public string Execute()
+        {
+            return "This is a result of an injection";
+        }
+    }
+
+    public interface IServiceOne
+    {
+        string Execute();
+    }
+}
